e7e3ab3133f265b4db552e292df99bd36e86b7a6
supermin199x <s.statiion@icloud.com>
2022-10-17 13:32:23 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Navbar} from './components/Navbar';
@@ -10,11 +11,11 @@ const App = () => {
     return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Navbar />}>
-				<Route index element={<Profile />} />
-				<Route path="Skills" element={<Skills />} />
-				<Route path="Projects" element={<Projects />} />
-				<Route path="Contacts" element={<Contacts />} />
+				<Route path="*" element={<Navbar />} >
+					<Route index element={<Profile />} />
+					<Route path="Skills" element={<Skills />} />
+					<Route path="Projects" element={<Projects />} />
+					<Route path="Contacts" element={<Contacts />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
