e06b06de8937e708186487f73dcfc728710865ba
supermin199x <s.statiion@icloud.com>
2022-10-17 13:40:18 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Navbar} from './components/Navbar';
 import {Profile} from './pages/Profile';
 import {Skills} from './pages/Skills';
