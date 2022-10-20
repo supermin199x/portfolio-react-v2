29f81b854a69fa899e0b6e71a66033fd69614c49
supermin199x <s.statiion@icloud.com>
2022-10-20 15:57:56 +0700

    add youtube

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,20 @@
 import React from "react";
+import '../css/Projects.css';
 import {Navbar} from '../components/Navbar';
+import {Title} from '../components/Title';
+import {Software} from '../components/Software';
 
 export function Projects(){
     return(
-		<div className="container">
-			<Navbar />
-			<div className="content">
-				PROJECTS
+		<div className="Projects-container">
+			<div className="Projects-show-navbar">
+				<Navbar />
+			</div>
+			<div className="Projects-show-content">
+				<Title />
+			</div>
+			<div className="Projects-show-post">
+				<Software />
 			</div>
 		</div>
     );
